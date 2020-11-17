fe912b5e39d5a671047de950faaba776b508b586
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2020-11-17 13:19:51 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-  $server = "localhost";
-  $user = "root";
-  $password = "";
-  $database = "dblatihan";
+  $server = "iwqrvsv8e5fz4uni.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+  $user = "uvt64mqamxp7nq3d";
+  $password = "wos56e0xbkecuewi";
+  $database = "htkp0ceu99llovg0";
 
   $koneksi = mysqli_connect($server, $user, $password, $database) or die (mysqli_error($koneksi));
 
